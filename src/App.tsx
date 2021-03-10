fd258ade768eb5215feef6047aeef7697ca4bc73
Garrison Snelling <garrison@hey.com>
2021-03-09 21:49:58 -0600

    Consolidating more logic to the store

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
 import { useEffect } from "react";
 import Board from "./components/Board";
 import Header from './components/Header';
-import { GameStore } from "./GameStore";
+import { declareWinner, GameStore } from "./GameStore";
 
-const possibleWin = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-];
+
 
 
 function App() {
 
   useEffect(() => {
-    const unsub = GameStore.subscribe(s => s.coordinates, (coordinates, allState) => {
-      if (allState.winner) return;
-
-      for (let i = 0; i < possibleWin.length; i++) {
-        const [a, b, c] = possibleWin[i];
-        if (coordinates[a] === coordinates[b] && coordinates[a] === coordinates[c]) {
-          GameStore.update(s => {
-            s.winner = coordinates[a]
-          })
-        }
-      }
-    })
+    const unsub = GameStore.subscribe(s => s.coordinates, declareWinner)
 
     return () => {
       unsub()
--- src/GameStore.tsx
+++ src/GameStore.tsx
@@ -30,4 +30,28 @@ function reset() {
   }))
 }
 
-export { updateCoordinates, GameStore, reset }
+const possibleWin = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+function declareWinner(coordinates: IGameStore['coordinates'], allState: IGameStore) {
+  if (allState.winner) return;
+
+  for (let i = 0; i < possibleWin.length; i++) {
+    const [a, b, c] = possibleWin[i];
+    if (coordinates[a] && coordinates[a] === coordinates[b] && coordinates[a] === coordinates[c]) {
+      GameStore.update(s => {
+        s.winner = coordinates[a]
+      })
+    }
+  }
+}
+
+export { updateCoordinates, GameStore, reset, declareWinner }
